22866bde36946fbbd060f99f3292d5ef55c70761
christopher boucher <chrismichb@yahoo.com>
2021-04-09 21:59:32 -0400

    added sudo code

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const calculate = (calculationSteps) => {}
+const calculate = (calculationSteps) => {
+    //run through array
+    //filter out extra
+    //push remaining to array
+    //if null set to 0
+    //convert string to integers
+    //solve equation
+    //return result
+}
 
 module.exports = calculate
