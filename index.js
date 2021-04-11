ae4941cff1d88c4c6ec741d74412753e5caa9fc8
christopher boucher <chrismichb@yahoo.com>
2021-04-11 18:51:48 -0400

    created variables for first and last index

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const calculate = (calculationSteps) => {
-  // push remaining to array
-  let calculation = []
-  // filter out extra
   let equation = calculationSteps.filter(extra => {
     return noNumber(extra)
   })
 
-  // run through array
+  // console.log(equation)
   for (let i = 0; i < equation.length; i++) {
-    switch (equation) {
-      case '*':
-        return answer = parseInt(equation[0]) * parseInt(equation[2])
-      case '/':
-        return answer = parseInt(equation[0]) / parseInt(equation[2])
-      case '+':
-        return answer = parseInt(equation[0]) + parseInt(equation[2])
-      case '-':
-        return answer = parseInt(equation[0]) - parseInt(equation[2])
+    let one = parseInt(equation[i])
+    let two = parseInt(equation[i = 2])
+    let operand = equation[i = 1]
+
+    // console.log(one)
+    // console.log(two)
+    switch (operand) {
+      case operand = '*':
+        let answerOne = one * two
+
+        return answerOne
+      case operand = '/':
+        let answerTwo = one / two
+
+        return answerTwo
+      case operand = '+':
+        let answerThree = one + two
+
+        return answerThree
+      case operand = '-':
+        let answerFour = one - two
+
+        return answerFour
       default:
         return NaN
     }
   }
-  // push remaining to array
-  // if null set to 0
-  // convert string to integers
-  // solve equation
-  // return result
 }
 
-// filter out extra
 const noNumber = (extra) => {
   switch (extra) {
     case '':
