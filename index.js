43039827328be010ddba244135908451517c65d0
christopher boucher <chrismichb@yahoo.com>
2021-04-10 20:29:36 -0400

    added switch statement

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const calculate = (calculationSteps) => {
-    //push remaining to array
-    let calculation = []
-    //run through array
-    for (let i = 0; i < calculationSteps.length; i++) {
-        //filter out extra
-        let equation = calculationSteps.filter(extra => {
-            return noNumber(extra)
-        })
-        return equation
+  // push remaining to array
+  let calculation = []
+  // filter out extra
+  let equation = calculationSteps.filter(extra => {
+    return noNumber(extra)
+  })
+
+  // run through array
+  for (let i = 0; i < equation.length; i++) {
+    switch (equation) {
+      case '*':
+        return answer = parseInt(equation[0]) * parseInt(equation[2])
+      case '/':
+        return answer = parseInt(equation[0]) / parseInt(equation[2])
+      case '+':
+        return answer = parseInt(equation[0]) + parseInt(equation[2])
+      case '-':
+        return answer = parseInt(equation[0]) - parseInt(equation[2])
+      default:
+        return NaN
     }
-    
-    //push remaining to array
-    //if null set to 0
-    //convert string to integers
-    //solve equation
-    //return result
+  }
+  // push remaining to array
+  // if null set to 0
+  // convert string to integers
+  // solve equation
+  // return result
 }
 
-//filter out extra
+// filter out extra
 const noNumber = (extra) => {
-    switch (extra) {
-        case '':
-        case 'foo':
-        case 'bar':
-        case undefined:
-            return false
-        default:
-            return true
-    }
+  switch (extra) {
+    case '':
+    case 'foo':
+    case 'bar':
+    case undefined:
+      return false
+    default:
+      return true
+  }
 }
+
 module.exports = calculate
